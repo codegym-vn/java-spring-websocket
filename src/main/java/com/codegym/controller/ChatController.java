51391c66a96590a5f57da6e985df6161839fcde8
Kieu Anh <vukieuanh.hnue@gmail.com>
2018-06-06 08:11:37 +0700

    init project

---
+++ src/main/java/com/codegym/controller/ChatController.java
@@ -0,0 +1,36 @@
+package com.codegym.controller;
+
+import com.codegym.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by rajeevkumarsingh on 24/07/17.
+ */
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+    @GetMapping("/")
+    public String show(){
+        return "index";
+    }
+
+}
